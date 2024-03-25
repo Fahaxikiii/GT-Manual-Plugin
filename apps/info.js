4f49f50ab6234abb53fde5ab6e37a1d59455dc12
绒布球 <93320886+Fahaxikiii@users.noreply.github.com>
2024-03-26 02:02:43 +0800

    Update info.js

--- apps/info.js
+++ apps/info.js
@@ -17,11 +17,11 @@ export class info extends plugin {
   
   async info() {
     let msg = [
-                  "http://jn.031117.xyz:23333/GTest/register"+'\n'
-                  "https://gt.wuliya.icu/GTest/register?key=wuliya"+'\n'
-                  "http://gt.yunzai.icu/GTest/register?key=免费开源项目-您如果是交易获得则是被骗-请及时退款并举报"+'\n'
-                  "http://v407.qingvps.cn:10099/GTest/register?key=biscuit"+'\n'
-                  "https://gt.161122.xyz/GTest/register"+'\n'
+                  "http://jn.031117.xyz:23333/GTest/register"+'\n'+
+                  "https://gt.wuliya.icu/GTest/register?key=wuliya"+'\n'+
+                  "http://gt.yunzai.icu/GTest/register?key=免费开源项目-您如果是交易获得则是被骗-请及时退款并举报"+'\n'+
+                  "http://v407.qingvps.cn:10099/GTest/register?key=biscuit"+'\n'+
+                  "https://gt.161122.xyz/GTest/register"+'\n'+
                   "https://gt.wanli.email/GTest/register"
     ]
     await this.reply(msg);
