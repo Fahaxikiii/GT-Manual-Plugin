2cc1ec2ce78741f815a04a70ad1d1f251961f576
绒布球 <93320886+Fahaxikiii@users.noreply.github.com>
2024-03-26 02:00:34 +0800

    Update info.js

--- apps/info.js
+++ apps/info.js
@@ -1,11 +1,9 @@
 import plugin from "../../../lib/plugins/plugin.js";
-import fs from "fs";
-import yaml from "js-yaml";
 
-export class api extends plugin {
+export class info extends plugin {
   constructor() {
     super({
-      name: "GT插件",
+      name: "GT接口信息",
       event: "message",
       priority: 33700,
       rule: [
@@ -16,12 +14,17 @@ export class api extends plugin {
       ],
     });
   }
-
-"http://jn.031117.xyz:23333/GTest/register"
-"https://gt.wuliya.icu/GTest/register?key=wuliya"
-"http://gt.yunzai.icu/GTest/register?key=免费开源项目-您如果是交易获得则是被骗-请及时退款并举报"
-"http://v407.qingvps.cn:10099/GTest/register?key=biscuit"
-"https://gt.161122.xyz/GTest/register"
-"https://gt.wanli.email/GTest/register"
   
+  async info() {
+    let msg = [
+                  "http://jn.031117.xyz:23333/GTest/register"+"/n"
+                  "https://gt.wuliya.icu/GTest/register?key=wuliya"
+                  "http://gt.yunzai.icu/GTest/register?key=免费开源项目-您如果是交易获得则是被骗-请及时退款并举报"
+                  "http://v407.qingvps.cn:10099/GTest/register?key=biscuit"
+                  "https://gt.161122.xyz/GTest/register"
+                  "https://gt.wanli.email/GTest/register"
+    ]
+    await this.reply("已将GT接口替换为接口0，请重启云崽后生效", true);
+  }
+
 }
