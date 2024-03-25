cddd2b5ec34eed4bce0af42c7c1381b62900ad52
绒布球 <93320886+Fahaxikiii@users.noreply.github.com>
2024-03-26 01:37:33 +0800

    Update api.js

--- apps/api.js
+++ apps/api.js
@@ -10,6 +10,10 @@ export class api extends plugin {
       priority: 33700,
       rule: [
         {
+          reg: /^#*(GT|gt)使用默认$/,
+          fnc: "api0",
+        },
+        {
           reg: /^#*(GT|gt)使用接口1$/,
           fnc: "api1",
         },
@@ -17,18 +21,32 @@ export class api extends plugin {
           reg: /^#*(GT|gt)使用接口2$/,
           fnc: "api2",
         },
+        {
+          reg: /^#*(GT|gt)使用接口3$/,
+          fnc: "api3",
+        },
       ],
     });
 
     this.configPath = "./plugins/GT-Manual-Plugin/config/gt.yaml";
   }
 
+  async api0() {
+    if (!this.e.isMaster) return false;
+
+    let config = yaml.load(fs.readFileSync(this.configPath, "utf8"));
+    config.verifyAddr = "http://jn.031117.xyz:23333/GTest/register";
+    config.signAddr = "ws://jn.031117.xyz:23333/mysSign";
+    fs.writeFileSync(this.configPath, yaml.dump(config));
+    await this.reply("已将GT接口替换为默认接口，请重启云崽后生效", true);
+  }
+  
   async api1() {
     if (!this.e.isMaster) return false;
 
     let config = yaml.load(fs.readFileSync(this.configPath, "utf8"));
-    config.verifyAddr = "https://gt.wuliya.top/GTest/register?key=wuliya";
-    config.signAddr = "wss://gt.wuliya.top/mysSign?key=wuliya";
+    config.verifyAddr = "https://gt.wuliya.icu/GTest/register?key=wuliya";
+    config.signAddr = "wss://gt.wuliya.icu/mysSign?key=wuliya";
     fs.writeFileSync(this.configPath, yaml.dump(config));
     await this.reply("已将GT接口替换为接口1，请重启云崽后生效", true);
   }
@@ -40,5 +58,14 @@ export class api extends plugin {
     config.signAddr = "ws://gt.yunzai.icu/mysSign?key=免费开源项目-您如果是交易获得则是被骗-请及时退款并举报";
     fs.writeFileSync(this.configPath, yaml.dump(config));
     await this.reply("已将GT接口替换为接口2,请重启云崽后生效", true);
+  }
+
+  async api3() {
+    if (!this.e.isMaster) return false;
+    let config = yaml.load(fs.readFileSync(this.configPath, "utf8"));
+    config.verifyAddr = "http://v407.qingvps.cn:10099/GTest/register?key=biscuit";
+    config.signAddr = "ws://gt.yunzai.icu/my";
+    fs.writeFileSync(this.configPath, yaml.dump(config));
+    await this.reply("已将GT接口替换为接口3,请重启云崽后生效", true);
   }
 }
